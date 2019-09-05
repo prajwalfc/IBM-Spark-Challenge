33cd50ecc74453612ea07c8ee2815fbe72521077
prajwalfc <prajwal_k@live.com>
2019-09-05 17:40:58 -0400

    cleanup and modified script

--- run_script.py
+++ run_script.py
@@ -7,10 +7,5 @@ os.system("chmod 777 -R output")
 if returned_value == 0:
     cmd = "cat output/temp/part-0000? >>output/final_output.txt"
     os.system(cmd)
-    # cmd = "sed 's/.//;s/,/  /;s/.$//' output/temp >> output/word_id"
-    # os.system(cmd)
-    # cmd = "cat output/word_index/part-0000? >>output/id_index"
-    # os.system(cmd)
-    # os.system("rm -r output/dict/ output/word_index/")
     os.system("rm -r output/temp")
 
